edcafa5071db2579bfa724d7f02c21939262e75f
HumanBojack <spychalar59@gmail.com>
2022-06-25 15:23:50 +0200

    began the connection with the model

--- App/brandseeker.py
+++ App/brandseeker.py
@@ -1,16 +1,32 @@
 import argparse
 # from models.yolov5 import utils
 # utils.notebook_init()
-# from models.yolov5.detect import run
+# from models.yolov5 import detect
 import torch
+from PIL import Image
+import cv2
 
 
 def predict(args):
     print(args)
 
     if args['model'] == 'yolo':
-        print('hello')
+        # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path='App/models/yolov5/best.pt')
+        # model = torch.hub.load('App/models/yolov5/', 'custom', path='App/models/yolov5/best.pt', source='local')
+
+        # img = Image.open("App/images/AWD_Thumbnail_2.png")
+        imgs = []
+        imgs.append(cv2.imread("App/images/AWD_Thumbnail_2.png"))
+        # img = Image.open("App/images/uber-eats-1024x682.jpg")
+        imgs.append(cv2.imread("App/images/uber-eats-1024x682.jpg"))
+        # img = Image.open("App/images/asus_g513qc_bb74_g15_ryzen_7_5800h_1633645.jpg")
+        imgs.append(cv2.imread("App/images/asus_g513qc_bb74_g15_ryzen_7_5800h_1633645.jpg"))
         # detect.run() # weights="App/models/yolov5/best.pt"
+
+        results = model(imgs, size=640)
+
+        results.print()
     
 
 
@@ -23,4 +39,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     predict(vars(args))
-
